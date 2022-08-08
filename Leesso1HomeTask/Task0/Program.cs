eea2b414c8b861b1bd5af465d14971f5328478bb
Evgeny Busleiko <busleiko@mail.ru>
2022-08-08 15:04:43 +0300

    Task0

--- Leesso1HomeTask/Task0/Program.cs
+++ Leesso1HomeTask/Task0/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine("Введите второе число");
+int b =Convert.ToInt32 (Console.ReadLine ());
+if (a<b)
+{
+    Console.WriteLine($"Максимум число {b}");
+}
+else if (a==b)
+{
+    Console.WriteLine($"Числа равны");
+}
+else Console.WriteLine($"Максимум число {a}");
