4f748e535c37f855fbd7a33c340d8d27aaf78866
Evgeny Busleiko <busleiko@mail.ru>
2022-08-08 15:56:17 +0300

    add task 3

---
+++ Leesso1HomeTask/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32 (Console.ReadLine ());
+for (int i = 1; i < N+1; i++)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine (i);
+    }
+}
+Console.WriteLine();
