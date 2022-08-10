c7be00e567752c9daf96493881385de145c9205d
Evgeny Busleiko <busleiko@mail.ru>
2022-08-10 09:29:48 +0300

    Add task 13

---
+++ lesson2hometask/tack13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите целое число :");
+int digit = Convert.ToInt32 (Console.ReadLine ());
+if (digit<100) Console.Write ("У числа нет третьей цифры");
+else 
+{
+while (digit>999)
+    {
+        digit=digit/10;    //Отбрасываем все цифры с конца до 3-ей
+    }
+digit=digit%10; // отделяем 3-ю
+Console.WriteLine (digit);
+}
--- lesson2hometask/task10/Program.cs
+++ lesson2hometask/task10/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine ("Введите целое трехзначное число :");
+﻿Console.Write ("Введите целое трехзначное число :");
 int dig3 = Convert.ToInt32 (Console.ReadLine ());
 int dig1 = dig3-dig3/100*100; // Убрал первую цифру
 dig1=dig1/10;
