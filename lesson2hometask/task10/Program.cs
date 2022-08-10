c439c31bed8a3a8a91aebede02296508afb158b1
Evgeny Busleiko <busleiko@mail.ru>
2022-08-10 09:13:08 +0300

    Add task 10

---
+++ lesson2hometask/task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine ("Введите целое трехзначное число :");
+int dig3 = Convert.ToInt32 (Console.ReadLine ());
+int dig1 = dig3-dig3/100*100; // Убрал первую цифру
+dig1=dig1/10;
+Console.WriteLine (dig1); 
