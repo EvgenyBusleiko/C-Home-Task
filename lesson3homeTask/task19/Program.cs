2afd654c2e2675ab23f9a1ad52641b737d81b68c
Evgeny Busleiko <busleiko@mail.ru>
2022-08-13 13:32:26 +0300

    Add task 19

---
+++ lesson3homeTask/task19/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear ();
+Console.Write ("Введите целое число : ");
+int N = Convert.ToInt32 (Console.ReadLine ());
+
+int CountDig (int Np)// определяем количество цифр в числе count
+{   int temp=Np;
+    int count =0;
+    while (temp>0)
+    {
+        temp=temp/10;
+        
+        count++ ;   
+    }
+    return count;
+}
+
+
+double Revers (int Np,int count)// переварачиваем число в обратном порядке
+{
+    double dig=0;
+    double AntiN=0;
+    for (int i = count; i >0; i--)
+    {
+        dig=(Np%10)*Math.Pow(10,i-1);
+        AntiN=AntiN+dig;
+        //Console.WriteLine (AntiN);
+        Np=Np/10;
+   
+    }
+    return AntiN;
+}   
+int V=CountDig (N);
+//Console.WriteLine (V);
+double VersusN=Revers (N,V);
+string str=(N==VersusN) ? ("полином") : ("НЕ полином"); // Сравниваем прямое и перевернутое число
+Console.WriteLine ($"Число {N} это {str}");
+
+
+
