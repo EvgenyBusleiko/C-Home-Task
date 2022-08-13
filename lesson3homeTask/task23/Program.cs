6a6ce877bc1c9741b44f2a31f56b097894e23154
Evgeny Busleiko <busleiko@mail.ru>
2022-08-13 08:42:01 +0300

    add task 23

---
+++ lesson3homeTask/task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear ();
+Console.Write ("Введите целое число : ");
+int N = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ($" X | X^3",3);
+Console.WriteLine("_____________");
+for (int i = 1; i < N+1; i++)
+{
+    Console.WriteLine ($"{i,3}|{i*i*i,3}");
+}
+Console.WriteLine();
