ff5eeb9e6b8601db956f3b54abd5ae42661a391e
Evgeny Busleiko <busleiko@mail.ru>
2022-08-18 12:03:04 +0300

    add task29

---
+++ Lesson4homeTask/task29/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear ();
+int [] RandArray (int len)
+{
+    int []arraytemp=new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arraytemp[i]=new Random().Next(0,100);
+    }
+    return arraytemp;
+}   
+void PrintArray (int [] res, int len)
+{   Console.Write ("[");
+    for (int i = 0; i < len-1; i++)
+        {
+        Console.Write ($"{res[i]},");
+        }
+        Console.WriteLine ($"{res[len-1]}]");
+}
+Console.Write("Введите количество элементов массива ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+if (a==0) Console.WriteLine("Пустой массив");
+    
+else
+{
+    int [] result= RandArray (a);
+    PrintArray (result,a);
+}
