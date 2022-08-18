6cc83fe5522cfa27e9221d4097bb8985fbdcaf47
Evgeny Busleiko <busleiko@mail.ru>
2022-08-18 11:04:27 +0300

    add task 25

---
+++ Lesson4homeTask/task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear ();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine ());
+
+int Degree (int ad, int bd)
+{
+    int mult=1;
+    for (int i = 1; i <= b; i++)
+    {
+        mult=mult*a;
+    
+    }
+    return mult;
+}   
+int multpl =Degree (a,b);
+Console.WriteLine ($"Число {a} в степени {b} = {multpl}");
