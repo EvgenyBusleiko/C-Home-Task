3b46fb8cb1bc9eeda455c95b311b643c0723e071
Evgeny Busleiko <busleiko@mail.ru>
2022-08-24 10:38:30 +0300

    add task 43

---
+++ Lesson6homeTask/Task43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear ();
+
+Console.WriteLine("Программа для поиска координат точки пересечения двух функций вида y=kХ+b ");
+Console.WriteLine();
+Console.Write("Введите коэффициент k первой фунции: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент b первой фунции: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент k второй фунции: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент b второй фунции: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x=((b2-b1)/(k1-k2));
+double y=k1*x+b1;
+Console.WriteLine ($"Функции пересекутся в точке с координатами ({x};{y})");
