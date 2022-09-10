64c77987ad4c2ab084ef0b09e4e57c8d660bdb0a
Evgeny Busleiko <busleiko@mail.ru>
2022-09-10 21:44:49 +0300

    Add task about recursion

---
+++ Lesson9HomeTask/task70/Program.cs
@@ -0,0 +1,32 @@
+﻿// 2. Задача на разворот числа:
+// Дано число multpl, десятичная запись которого не содержит нулей.
+//  Получите число, записанное теми же цифрами, но в противоположном порядке.
+
+int Check (string textToPrint)
+{
+    int checkImput=-1;
+    do
+    {   
+        Console.Write($"Введите {textToPrint}: ");
+        checkImput = Convert.ToInt32(Console.ReadLine());
+        if (checkImput<0) Console.WriteLine("Ошибка: Число должно быть положительным! Если хотите выйти введите, пожалуйста '0'");
+        else if (checkImput==0) return checkImput=0;
+    
+    }
+    while (checkImput<0);
+    return checkImput;
+
+}
+int Reverse (int num,int multpl)
+{
+    
+    if (num<10) return 10*multpl+num;
+    return Reverse (num/10,10*multpl+num%10);
+       
+}
+
+
+Console.WriteLine ("Программа для разворота числа с помощью рекурсии");
+int number=Check ("целое положительное число");
+int revers=Reverse (number,0);
+Console.WriteLine ($"{number} -> {revers}"); 
---
+++ Lesson9HomeTask/task72/Program.cs
@@ -0,0 +1,39 @@
+﻿// 1. Дано слово, состоящее только из строчных латинских букв.
+//  Проверить, является ли это слово палиндромом. Вывести "Да" или "Нет".
+
+string CheckNullStrins ()
+{
+    string stringToCheck="";
+    do
+    {   
+        Console.Write("Введите слово из латинских строчных букв:  ");
+        stringToCheck = Console.ReadLine();
+        if (stringToCheck=="") Console.WriteLine("Ошибка: Введена пустая строка Если хотите выйти введите, пожалуйста 'E'");
+        else if (stringToCheck=="E") return stringToCheck="E";
+    
+    }
+    while (stringToCheck=="");
+    return stringToCheck;
+
+}
+
+string ReversString (string wordToRevers)
+
+{
+    if (wordToRevers=="") return wordToRevers;
+    else return ReversString(wordToRevers.Substring(1)) + wordToRevers[0];
+}
+
+Console.WriteLine ("Программа для определения, является ли слово полиномом");
+
+string word = CheckNullStrins ();
+
+
+if (word!="E")
+{
+    string word_UP=word.ToUpper();
+    string reversed_Word=ReversString (word_UP); 
+
+    if (word_UP==reversed_Word) Console.WriteLine ($"{word_UP} наоборот будет {reversed_Word} и это ПОЛИНОМ!");
+    else Console.WriteLine ($"{word_UP} наоборот будет {reversed_Word} и это НЕ ПОЛИНОМ!");
+}
